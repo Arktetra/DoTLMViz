09bf9985b286923a4a6912d8fc0c69ea4c679e1c
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-02-27 13:56:50 +0545

    add: dropdown basic component

---
+++ frontend/src/components/DropDown.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let { options, label = "", ostyle = "", onChangeCb = null } = $props()
+
+    const onOptChange = (e: any) => {
+        e.preventDefault();
+        const sel = e.target.value;
+        if(onChangeCb) onChangeCb(sel)
+    }
+
+</script>
+
+<div class="w-full">
+    {#if label}
+        <span class="text-sm text-theme font-bold">{label}</span>
+    {/if}
+    <select class="w-full mt-2 accent-theme text-sm p-1 px-2 {ostyle}" onchange={onOptChange}>
+        {#each options as opt, ind}
+            <option selected={ind == 0 ? true : false}>{opt}</option>
+        {/each}
+    </select>
+</div>
