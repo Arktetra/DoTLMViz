9feaf75b7832d2c26407a7bbed1369c063aa3c6a
Arktetra <sameershrestha.sm@gmail.com>
2025-03-03 19:54:51 +0545

    add setup for markdown to svelte rendering

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+import { mdsvex } from 'mdsvex';
+import remarkMath from 'remark-math';
+import rehypeKatexSvelte from 'rehype-katex-svelte';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: [".md"],
+	remarkPlugins: [remarkMath],
+	rehypePlugins: [rehypeKatexSvelte]
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: [".svelte", ".md"],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
