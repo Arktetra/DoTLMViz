9c974fe20659e90b190eb2517873030a74e8f30b
Arktetra <sameershrestha.sm@gmail.com>
2025-03-08 13:42:39 +0545

    minor changes

--- frontend/src/routes/readings/[slug]/+page.ts
+++ frontend/src/routes/readings/[slug]/+page.ts
@@ -4,7 +4,6 @@ import type { PageLoad } from "../../$types";
 export const load: PageLoad = async ({ params }) => {
     try {
         const reading = await import(`../../../readings/${params.slug}.md`);
-        console.log(params);
         return {
             content: reading.default,
             meta: reading.metadata
